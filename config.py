e1b3ba95f361ae08f95f8580ce5dde6a247d2f70
July <gimh64627@gmail.com>
2025-10-21 16:20:31 +0900

    config file modify

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from typing import Optional
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base
+
 
 class BaseConfig(BaseSettings):
     DB_URL: Optional[str]
@@ -9,12 +9,7 @@ class BaseConfig(BaseSettings):
     CLOUDINARY_SECRET_KEY: Optional[str]
     CLOUDINARY_API_KEY: Optional[str]
     CLOUDINARY_CLOUD_NAME: Optional[str]
+
+    """Loads the dotenv file. Including this is necessary to get
+    pydantic to load a .env file."""
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
-
-Base = declarative_base()
-
-class CarModel(Base):
-    __tablename__ = "cars"
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    manufacturer = Column(String)
